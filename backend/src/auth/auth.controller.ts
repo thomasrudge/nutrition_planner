ef141c17788357010abf899fb008ca953cbb4fc1
thomasrudge <thomasfr@al.insper.edu.br>
2026-03-10 14:41:48 -0300

    feat: implement JWT guard with public decorator for route protection

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/users/DTOs/create-user.dto';
 import { SigninDto } from './DTOs/signin.dto';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -10,11 +11,13 @@ export class AuthController {
     ){}
 
     @Post('/signup')
+    @Public()
     async signup(@Body() body: CreateUserDto){
         return this.authService.signup(body.email, body.password, body.name)
     }
 
     @Post('/signin')
+    @Public()
     async signin(@Body() body: SigninDto){
         return this.authService.signin(body.email, body.password)
     }
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Module({
-  providers: [AuthService],
+  providers: [AuthService,JwtStrategy, 
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    }
+  ],
   controllers: [AuthController],
   imports: [UsersModule,
     JwtModule.register({
